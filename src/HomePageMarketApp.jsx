2588db88e5a598c664871bdaf3a482666a50d69f
Nanni <danitza.p.96@gmail.com>
2024-08-30 16:00:20 -0400

    Feature Carrusel

--- src/HomePageMarketApp.jsx
+++ src/HomePageMarketApp.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Navbar } from './components/Navbar'
+import { Carrusel } from './components/Carrusel'
 
 export const HomePageMarketApp = () => {
   return (
     <>
         <Navbar />
+        <div className='container'>
+          <Carrusel />
+        </div>
     </>
   )
 }
